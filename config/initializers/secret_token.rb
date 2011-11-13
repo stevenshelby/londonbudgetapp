32d51f6dec403ce009770d6f5a13d66133382256
Steven Shelby <stevenshelby@gmail.com>
2011-11-12 23:23:55 -0500

    added the rest of the default rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Londonbudgetapp::Application.config.secret_token = '416a762a82940e7db6ed87820a14f9fd3ddb22ee75a4cd4bf59f79020733ac936daed8465b6ae737af1a42e13d37b5ee495bcca649591829154e87b98e713dfa'
